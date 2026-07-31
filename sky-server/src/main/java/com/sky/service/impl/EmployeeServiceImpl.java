75e3c19efc43be5652cf7efa9a8b598341c68689
huli <3378604506@qq.com>
2026-07-31 10:33:56 +0800

    add md5 digest for user password

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -38,8 +38,11 @@ public class EmployeeServiceImpl implements EmployeeService {
             throw new AccountNotFoundException(MessageConstant.ACCOUNT_NOT_FOUND);
         }
 
+
+        // 对前端传进来的明文密码进行md5加密
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
